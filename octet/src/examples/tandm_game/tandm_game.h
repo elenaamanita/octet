7791df96cc90622f34e0e947b7dacb59f89d57da
BaggioPereira <baggio_111111@hotmail.com>
2015-10-02 22:08:15 +0100

    Txt file loading

--- octet/src/examples/tandm_game/tandm_game.h
+++ octet/src/examples/tandm_game/tandm_game.h
@@ -4,20 +4,67 @@
 //
 // Modular Framework for OpenGLES2 rendering on multiple platforms.
 //
+
+#include <fstream>
+#include <sstream>
+
 namespace octet {
   /// Scene containing a box with octet.
   class tandm_game : public app {
     // scene for drawing box
     ref<visual_scene> app_scene;
+	random r;
+
+	string contents;
+	char charArray[4][10];
+
+	int levelNumber = 1;
   public:
     /// this is called when we construct the class before everything is initialised.
     tandm_game(int argc, char **argv) : app(argc, argv) {
     }
 
+	//clear the scene
+	void newScene()
+	{
+		app_scene->reset();
+		app_scene->create_default_camera_and_lights();
+	}
+
+	//read txt file and get level data
+	void loadTxt()
+	{
+		std::fstream myFile;
+		std::stringstream fileName;
+		fileName << "level" << levelNumber << ".txt";
+		myFile.open(fileName.str().c_str(), std::ios::in);
+		if (!myFile.is_open())
+		{
+			printf("File not opened/is missing\n");
+		}
+
+		else
+		{
+			printf("File open\n");
+			myFile.seekg(0, myFile.end);
+			int length = myFile.tellg();
+			myFile.seekg(0, myFile.beg);
+
+			std::stringstream file;
+			file.str(std::string());
+			file << myFile.rdbuf();
+			myFile.close();
+
+			contents = file.str().c_str();
+			printf("%s\n", contents.c_str());
+		}
+	}
+
     /// this is called once OpenGL is initialized
     void app_init() {
       app_scene =  new visual_scene();
-      app_scene->create_default_camera_and_lights();
+	  newScene();
+	  loadTxt();
     }
 
     /// this is called to draw the world
